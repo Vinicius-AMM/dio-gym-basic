382e49894d0def6f625194e2125b5fd25012b8cc
Vinicius Aurélio <aurelioviniciusprofissional@gmail.com>
2024-11-08 18:26:59 -0300

    Initial commit

---
+++ src/main/java/dio/gym/DioGymApplication.java
@@ -0,0 +1,13 @@
+package dio.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/gym/DioGymApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
