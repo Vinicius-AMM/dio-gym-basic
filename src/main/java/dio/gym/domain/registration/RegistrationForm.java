21ff5a2d9ece447269b244918d96dc9ecf4959cb
Vinicius Aurélio <aurelioviniciusprofissional@gmail.com>
2024-11-08 23:44:12 -0300

    feat : create RegistrationForm class

---
+++ src/main/java/dio/gym/domain/registration/RegistrationForm.java
@@ -0,0 +1,14 @@
+package dio.gym.domain.registration;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistrationForm {
+    @NotNull
+    private Long memberId;
+}
