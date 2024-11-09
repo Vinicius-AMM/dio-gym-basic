6f9bc01b9df55126f4c6729dfdf649357ebaa6fc
Vinicius Aurélio <aurelioviniciusprofissional@gmail.com>
2024-11-08 23:42:10 -0300

    feat : create FitnessAssessmentUpdateForm class

---
+++ src/main/java/dio/gym/domain/fitness_assessment/FitnessAssessmentUpdateForm.java
@@ -0,0 +1,11 @@
+package dio.gym.domain.fitness_assessment;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FitnessAssessmentUpdateForm {
+    private Double weight;
+    private Double height;
+}
